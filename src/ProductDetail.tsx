3fc7b5ac06ce96942e4a3e48dab88950473b36ee
Travis CI <travis@travis-ci.org>
2021-10-08 12:33:06 +0200

    Introduce useProduct hook & ProductDetail slots

--- src/ProductDetail.tsx
+++ src/ProductDetail.tsx
@@ -1,36 +1,48 @@
-import { useContext } from "react";
+import { useContext, ReactNode } from "react";
 import { CartContext } from "./Cart";
 import { formatPrice } from "./helpers";
 import { ProductRecord } from "./types";
 
 export function ProductDetail(props: ProductDetailProps) {
+  const { product, image } = props;
 
-    const { product } = props;
+  const cartContext = useContext(CartContext);
 
-    const cartContext = useContext(CartContext);
+  const handleAddToCart = () => {
+    cartContext?.addItem(product.id, 1);
+  };
 
-    const handleAddToCart = () => {
-        cartContext?.addItem(product.id, 1);
-    }
-
-    return (
-        <article className="product-item" itemScope itemType="http://schema.org/Product">
-
-            <div className="max-w-sm bg-white shadow-lg rounded overflow-hidden m-auto">
-                <img className="w-full" src={product.image} alt={product.reference} />
-                <div className="p-4">
-                    <div className="text-gray-900 font-bold text-2xl">{product.reference}</div>
-                    <p className="mt-2 text-gray-600 text-sm">{product.description}</p>
-                </div>
-                <div className="p-4 flex item-center justify-between mt-3">
-                    <h1 className="text-gray-700 font-bold text-xl">{formatPrice(product.price)}</h1>
-                    <button onClick={handleAddToCart} className="px-3 py-2 bg-gray-800 text-white text-xs font-bold uppercase rounded">Add to cart</button>
-                </div>
-            </div>
-        </article>
-    );
+  return (
+    <article
+      className="product-item"
+      itemScope
+      itemType="http://schema.org/Product"
+    >
+      <div className="max-w-sm bg-white shadow-lg rounded overflow-hidden m-auto">
+        {image}
+        <div className="p-4">
+          <div className="text-gray-900 font-bold text-2xl">
+            {product.reference}
+          </div>
+          <p className="mt-2 text-gray-600 text-sm">{product.description}</p>
+        </div>
+        <div className="p-4 flex item-center justify-between mt-3">
+          <h1 className="text-gray-700 font-bold text-xl">
+            {formatPrice(product.price)}
+          </h1>
+          <button
+            onClick={handleAddToCart}
+            className="px-3 py-2 bg-gray-800 text-white text-xs font-bold uppercase rounded"
+          >
+            Add to cart
+          </button>
+        </div>
+      </div>
+    </article>
+  );
 }
 
 type ProductDetailProps = {
-    product: ProductRecord
-}
+  product: ProductRecord;
+  image: ReactNode;
+};
--- src/ProductPage.tsx
+++ src/ProductPage.tsx
@@ -1,45 +1,99 @@
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router';
-import { getProductById } from './api';
-import { ProductDetail } from './ProductDetail';
-import { SkeletonItem } from './SkeletonItem';
-import { ProductRecord } from './types';
+import { useCallback, useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { getProductById } from "./api";
+import { ProductDetail } from "./ProductDetail";
+import { SkeletonItem } from "./SkeletonItem";
+import { ProductRecord } from "./types";
 
 export interface IProductDetailRouteParams {
-    id: string;
+  id: string;
 }
 
+const useProduct = (
+  id: string
+): [ProductRecord | undefined, boolean, Error | undefined, () => void] => {
+  const [isLoading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<Error>();
+  const [product, setProduct] = useState<ProductRecord>();
+
+  const refetch = useCallback(() => {
+    getProductById(id)
+      .then((response) => {
+        setProduct(response);
+      })
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
+  }, [id]);
+
+  useEffect(() => {
+    refetch();
+  }, [refetch]);
+
+  return [product, isLoading, error, refetch];
+};
+
 export function ProductPage() {
-    
-    const [isLoading, setLoading] = useState<Boolean>(true);
-    const [product, setProduct] = useState<ProductRecord>();
+  let { id } = useParams<IProductDetailRouteParams>();
+  const [product, isLoading, error, refetch] = useProduct(id);
 
-    let { id } = useParams<IProductDetailRouteParams>();
-
-    useEffect(() => {
-        getProductById(id).then((response) => {
-            setProduct(response);
-            setLoading(false);
-        });
-    }, [id]);
-
-    if(isLoading) {
-        return (
-            <article className="product-item" itemScope itemType="http://schema.org/Product">
-                <div className="max-w-sm bg-white shadow-lg rounded overflow-hidden m-auto">
-                    <SkeletonItem className="product-image w-full" style={{width: '384px', height: '288px'}} />
-                    <div className="p-4">
-                        <div className="text-gray-900 font-bold text-2xl"><SkeletonItem className="product-reference" style={{width: '300px'}} /></div>
-                        <SkeletonItem className="product-description mt-2 text-gray-600 text-sm" style={{width: '300px', height: '80px'}} />
-                    </div>
-                    <div className="p-4 flex item-center justify-between mt-3">
-                        <h1 className="text-gray-700 font-bold text-xl"><SkeletonItem className="product-price" style={{width: '100px'}} /></h1>
-                        <SkeletonItem className="product-button" style={{width: '100px', height: '32px'}} />
-                    </div>
-                </div>
-            </article>
-        );
-    } else {
-        return <ProductDetail product={product} />;
-    }
+  if (isLoading) {
+    return (
+      <article
+        className="product-item"
+        itemScope
+        itemType="http://schema.org/Product"
+      >
+        <div className="max-w-sm bg-white shadow-lg rounded overflow-hidden m-auto">
+          <SkeletonItem
+            className="product-image w-full"
+            style={{ width: "384px", height: "288px" }}
+          />
+          <div className="p-4">
+            <div className="text-gray-900 font-bold text-2xl">
+              <SkeletonItem
+                className="product-reference"
+                style={{ width: "300px" }}
+              />
+            </div>
+            <SkeletonItem
+              className="product-description mt-2 text-gray-600 text-sm"
+              style={{ width: "300px", height: "80px" }}
+            />
+          </div>
+          <div className="p-4 flex item-center justify-between mt-3">
+            <h1 className="text-gray-700 font-bold text-xl">
+              <SkeletonItem
+                className="product-price"
+                style={{ width: "100px" }}
+              />
+            </h1>
+            <SkeletonItem
+              className="product-button"
+              style={{ width: "100px", height: "32px" }}
+            />
+          </div>
+        </div>
+      </article>
+    );
+  } else {
+    return (
+      <ProductDetail
+        product={product}
+        image={
+          isLoading ? (
+            <SkeletonItem
+              className="product-image w-full"
+              style={{ width: "384px", height: "288px" }}
+            />
+          ) : (
+            <img
+              className="w-full"
+              src={product?.image}
+              alt={product?.reference}
+            />
+          )
+        }
+      />
+    );
+  }
 }
